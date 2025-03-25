b46aab7a94e21b0ed028ea59ab9566bac224fcc5
junhyuk <sojourn8th@naver.com>
2025-03-25 23:14:01 +0900

    바이러스 백준문제

--- BOJ/boj.py
+++ BOJ/boj.py
@@ -1,6 +1,24 @@
-score=[]
+import sys
+input=sys.stdin.readline
 
-for i in range(2):
-    T,F,S,P,C=map(int,input().split())
-    score.append(T*6+F*3+S*2+P+C*2)
-print(score[0],score[1])
+N=int(input())
+M=int(input())
+
+network=[[] for i in range(N+1)]
+
+for i in range(M):
+    a,b=map(int,input().split())
+    network[a].append(b)
+    network[b].append(a)
+
+
+list1=[0]*(N+1)
+def DFS(warm):
+    list1[warm]=1
+    count=0
+    for i in network[warm]:
+        if not list1[i]:
+            count+=1+ DFS(i)
+    return count
+
+print(DFS(1))
