775bbd64b276227b56ec6b72cb86393be0e70058
junhyuk <sojourn8th@naver.com>
2025-03-29 23:58:36 +0900

    방학숙제

--- BOJ/boj.py
+++ BOJ/boj.py
@@ -2,7 +2,22 @@ import sys
 
 input=sys.stdin.readline
 
-N=int(input())
+L= int(input())
+A= int(input())
+B= int(input())
+C= int(input())
+D= int(input())
 
-for i in range(N,0,-1):
-    print('*'*i)
+korean=A//C
+math=B//D
+
+if korean>math:
+    if A%C==0:
+        print(L-korean)
+    else:
+        print(L-1-korean)
+else:
+    if B%D==0:
+        print(L-math)
+    else:
+        print(L-1-math)
