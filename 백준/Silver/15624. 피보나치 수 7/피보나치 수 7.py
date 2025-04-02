c456443aebaf73cf83b8c29911801f374488a915
JUN HYUK KWON <68374065+Diame58@users.noreply.github.com>
2025-04-02 22:52:29 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 248 ms, Memory: 32412 KB, Score: 201 point -BaekjoonHub

--- 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
@@ -1,11 +1,5 @@
-import sys
-
-input=sys.stdin.readline
-
-N=int(input())
-
-mod=1000000007
-x,y=0,1
-for i in range(2,N+1):
-    x,y=y,(x+y)%mod
-print(y)
+n = int(input())
+a, b = 0, 1
+for i in range(n):
+    a, b = (a+b)%1000000007, a%1000000007
+print(a)
