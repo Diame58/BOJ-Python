917c0092e4de5323e80cc4aaf4145d6596428fb4
junhyuk <sojourn8th@naver.com>
2025-04-04 21:25:28 +0900

    4방향 탐색 알고리즘 dfs

--- BOJ/boj.py
+++ BOJ/boj.py
@@ -1,23 +1,37 @@
 import sys
-
+sys.setrecursionlimit(10000)
 input=sys.stdin.readline
 
-L= int(input())
-A= int(input())
-B= int(input())
-C= int(input())
-D= int(input())
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
 
-korean=A//C
-math=B//D
+def dfs(x, y):
+    bat[y][x] = 0
 
-if korean>math:
-    if A%C==0:
-        print(L-korean)
-    else:
-        print(L-1-korean)
-else:
-    if B%D==0:
-        print(L-math)
-    else:
-        print(L-1-math)
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if 0 <= nx < M and 0 <= ny < N and bat[ny][nx] == 1:
+            dfs(nx, ny)
+
+T = int(input())
+
+for _ in range(T):
+    M, N, K = map(int, input().split())
+
+    bat = [[0 for _ in range(M)] for _ in range(N)]
+
+    for _ in range(K):
+        x, y = map(int, input().split())
+        bat[y][x] = 1
+
+    worm_count = 0
+
+    for y in range(N):
+        for x in range(M):
+            if bat[y][x] == 1:
+                dfs(x, y)
+                worm_count += 1
+
+    print(worm_count)
