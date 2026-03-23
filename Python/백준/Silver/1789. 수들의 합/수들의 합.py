055ffde08f6bb0eff39d6b58c62ef130707a0662
JUN HYUK KWON <68374065+Diame58@users.noreply.github.com>
2026-03-23 16:50:27 +0900

    [Silver V] Title: 수들의 합, Time: 44 ms, Memory: 32544 KB -BaekjoonHub

--- Python/백준/Silver/1789. 수들의 합/수들의 합.py
+++ Python/백준/Silver/1789. 수들의 합/수들의 합.py
@@ -1,10 +1,10 @@
-# 서로 다른 N개의 자연수의 합이 S라고 한다. S를 알 때, 자연수 N의 최댓값은 얼마일까?
 S = int(input())
-total = 0
-N = 0
-while True:
-    N += 1
-    if total + N > S:
-        break
-    total += N
-print(N - 1)
+left = 1
+right = S
+while left <= right:
+    mid = (left + right) // 2
+    if mid * (mid + 1) // 2 <= S:
+        left = mid + 1
+    else:
+        right = mid - 1
+print(right)
