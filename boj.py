c41db280dd37dd3796285340359ebb094ab03c98
junhyuk <sojourn8th@naver.com>
2026-03-25 00:13:19 +0900

    03.25

--- boj.py
+++ boj.py
@@ -1,28 +1,12 @@
-#2
+docu = input()
+voca = input()
+voca_len = len(voca)
+cnt=0
 
-import pandas as pd
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.model_selection import train_test_split
-
-train=pd.read_csv('https://raw.githubusercontent.com/YoungjinBD/data/main/exam/10_2_train.csv')
-test=pd.read_csv('https://raw.githubusercontent.com/YoungjinBD/data/main/exam/10_2_test.csv')
-
-print(train.info())
-print(test.info())
-
-train_x=train.drop(['gas_total'], axis=1)
-train_y=train['gas_total']
-test_x=test.drop(['gas_total'], axis=1)
-test_y=test['gas_total']
-
-train_x,train_y=train_test_split(train_x,train_y,test_size=0.2,random_state=42)
-
-cate_cols=train_x.select_dtypes(include=['object']).columns
-num_cols=train_x.select_dtypes(include=['number']).columns
-
-encoder=OneHotEncoder(sparse=False, handle_unknown='ignore')
-train_x[cate_cols]=encoder.fit_transform(train_x[cate_cols])
-test_x[cate_cols]=encoder.transform(test_x[cate_cols])
-
-
-
+while len(docu) >= voca_len:
+    if docu[:voca_len] == voca:
+        docu= docu[voca_len:]
+        cnt+=1
+    else:
+        docu=docu[1:]
+print(cnt)
